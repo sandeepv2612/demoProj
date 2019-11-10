7ad487be4e4932189e04fd89a52729150b336e7e
sande <sande@192.168.0.98>
2019-11-10 21:39:39 +0530

    First Commit

---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Testing 123..");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Test();
+	}
+
+}
