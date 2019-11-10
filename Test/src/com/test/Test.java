5da661b0b364bcfd4a0d525edfa6ac21fe0bda27
sande <sande@192.168.0.98>
2019-11-10 23:21:38 +0530

    goku commit 1

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -5,8 +5,7 @@ public class Test {
 	public Test() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Testing 123..");
-		System.out.println("Code by code_goku..");
-		
+		System.out.println("Code by code_goku..");		
 	}
 
 	public static void main(String[] args) {
